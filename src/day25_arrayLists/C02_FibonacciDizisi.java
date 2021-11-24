888fa4e416c73a0f2eb986aa94d7c09de208cddc
Emrh SGLM <emrh.sglm7168@gmail.com>
2021-11-24 22:53:18 +0300

    23.11.2021 22:53

---
+++ src/day25_arrayLists/C02_FibonacciDizisi.java
@@ -0,0 +1,32 @@
+package day25_arrayLists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C02_FibonacciDizisi {
+
+	public static void main(String[] args) {
+		// verilen sayidan, kucuk Fibonacci sayilarini bir list olarak kaydedelim
+		
+		// 1,1,2,3,5,8,13,21,34,55,89,......
+		
+		int sinir=100;
+		
+		List <Integer> fibonacci = new ArrayList<>();
+		
+		fibonacci.add(1);
+		fibonacci.add(1);
+		int sayi=0;
+		
+		while (sayi < sinir ) {
+			sayi = fibonacci.get(fibonacci.size()-2) +fibonacci.get(fibonacci.size()-1) ;
+			fibonacci.add(sayi);
+		}
+		System.out.println(fibonacci);
+		fibonacci.remove(fibonacci.size()-1);
+		System.out.println(fibonacci);
+		
+
+	}
+
+}
