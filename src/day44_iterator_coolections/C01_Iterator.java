65ceb2b93afc13b9f6d9202789cf8ffe1139e582
EmrhSGLM <emrh.sglm7168@gmail.com>
2021-12-17 23:01:03 +0300

    43.ders arasi

---
+++ src/day44_iterator_coolections/C01_Iterator.java
@@ -0,0 +1,38 @@
+package day44_iterator_coolections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C01_Iterator {
+
+	public static void main(String[] args) {
+		
+		List <Integer> liste = new ArrayList<>();
+		
+		liste.add(5);
+		liste.add(3);
+		liste.add(7);
+		liste.add(1);
+		liste.add(9);
+		liste.add(8);
+		liste.add(2);
+		
+		System.out.println(liste); // [5, 3, 7, 1, 9, 8, 2]
+		
+		// index kullanmadan tum elemanlari 3 ekleyenmis olarak yazdirin
+		int i = 0;
+		for (Integer each : liste) {
+			each+=3;
+			liste.add(i, each);
+			i++;
+			//System.out.print(each+3 + " ");
+		}
+		
+		System.out.println("");
+		System.out.println(liste); // [5, 3, 7, 1, 9, 8, 2]
+		
+		
+
+	}
+
+}
